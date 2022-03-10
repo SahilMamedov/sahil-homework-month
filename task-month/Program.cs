0311145d5028b99a7f02a730dc4dac310c461132
SahilMamedov <sahilim@code.edu.az>
2022-03-10 15:26:45 +0400

    feat:homework-month

---
+++ task-month/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace task_month
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // task - Ay(month) adı daxil edilir. Çıxışa hansı fəsil olduğunu yazdırsın
+            // tapshirig if else and switch case ile hell olunmushdur.
+
+            string month;
+            month = Convert.ToString(Console.ReadLine());
+            
+
+           /* if (month == "dekabr" || month == "yanvar" || month == "fevral")
+            {
+                Console.WriteLine("Qish fesilidir");
+            }
+            else if (month == "mart" || month == "aprel" || month == "may")
+            {
+                Console.WriteLine("Yaz fesilidir");
+            }
+            else if (month == "iyun" || month == "iyul" || month == "avqust")
+            {
+                Console.WriteLine("Yay fesilidir");
+            }
+            else if (month == "sentyabr" || month == "oktyabr" || month == "noyabr")
+            {
+                Console.WriteLine("Payiz fesilidir");
+            }
+            else
+            {
+                Console.WriteLine("Bele bir ay movcud deyil.");
+            }
+
+           */
+            
+
+          /* switch(month)
+            {
+                case "dekabr":
+                case "yanvar":
+                case "fevral":
+                    Console.WriteLine("Qish Fesilidir");
+                    break;
+
+                case "mart":
+                case "aprel":
+                case "may":
+                    Console.WriteLine("Yaz Fesilidir");
+                    break;
+
+                case "iyun":
+                case "iyul":
+                case "avqust":
+                    Console.WriteLine("Yay Fesilidir");
+                    break;
+
+                case "sentyabr":
+                case "oktyabr":
+                case "noyabr":
+                    Console.WriteLine("Payiz Fesilidir");
+                    break;
+
+                default: Console.WriteLine("Bele bir ay movcud deyil");
+                    break; 
+            }    
+          
+          */
+
+        } 
+    }
+}
+          
